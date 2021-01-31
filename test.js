3a3b1ea71bab62348ce5944bdc377822699f7892
Botond Kulcsar <bkulcsar@gmx.com>
2021-01-31 23:13:25 +0200

    just started off with node

---
+++ test.js
@@ -0,0 +1,18 @@
+const name = 'mario';
+// console.log(name);
+// console.log(global);
+
+// setTimeout(() => {
+//     console.log('in the timeout');
+//     clearInterval(int);
+// }, 3000)
+
+// const int = setInterval(() => {
+//     console.log('int the interval');
+// }, 1000);
+
+// console.log(__dirname);
+// console.log(__filename);
+
+const os = require('os');
+console.log(os.platform(), os.homedir());
