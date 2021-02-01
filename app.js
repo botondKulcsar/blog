da06abd4e445b6211d7c0d31af551b45a73dd19d
Botond Kulcsar <bkulcsar@gmx.com>
2021-02-01 19:16:21 +0200

    installed ejs

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews');
+
 // listen for requests
 app.listen(3000);
 
