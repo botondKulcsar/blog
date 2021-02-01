03012cbc90848a84826056ceca47376bf1acac35
Botond Kulcsar <bkulcsar@gmx.com>
2021-02-01 22:24:38 +0200

    added static middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 // express app
 const app = express();
@@ -9,6 +10,12 @@ app.set('view engine', 'ejs');
 
 // listen for requests
 app.listen(3000);
+
+// middleware & static files
+app.use(express.static('public'));
+
+app.use(morgan('dev'));
+
 
 app.get('/', (req, res) => {
     // res.send('<p>home page</p>');
