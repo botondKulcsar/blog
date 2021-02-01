c34ab936f121655b40c39f59aa417f23be08029c
Botond Kulcsar <bkulcsar@gmx.com>
2021-02-01 21:18:35 +0200

    added partials

--- app.js
+++ app.js
@@ -12,20 +12,29 @@ app.listen(3000);
 
 app.get('/', (req, res) => {
     // res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname });
+    const blogs = [
+        { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit aemt consectetur' },
+        { title: 'Mario finds eggs', snippet: 'Lorem ipsum dolor sit aemt consectetur' },
+        { title: 'How to defeat browser', snippet: 'Lorem ipsum dolor sit aemt consectetur' }
+    ];
+    res.render('index', { title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
     // res.send('<p>about page</p>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about', { title: 'About' });
 });
 
 // redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-});
+// app.get('/about-us', (req, res) => {
+//     res.redirect('/about');
+// });
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new Blog' });
+})
 
 // 404
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404', { title: '404' });
 });
