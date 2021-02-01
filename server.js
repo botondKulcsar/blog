d680dd3131c4521a7b625e801bb6d2e3e0eae3b9
Botond Kulcsar <bkulcsar@gmx.com>
2021-02-01 15:46:30 +0200

    installed nodemon and lodash. added .gitignore

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
+    //lodash
+    const num = _.random(0, 20);
+    console.log(num);
+
+    const greet = _.once(() => {
+        console.log('hello');
+    });
+
+    greet();
+    greet(); // it's not gonna run, ONLY ONCE, because we set it so with _.once() method
 
     // set header content type
     res.setHeader('Content-Type', 'text/html');
